b90454c56d5f6ae303cc8c9ac5d9af5457acde0f
Mike Schinkel <mike@newclarity.net>
2023-11-18 20:39:05 -0500

    Made `want` values tighter w/recent changes

--- node_marshaler_test.go
+++ node_marshaler_test.go
@@ -46,7 +46,7 @@ func TestNodeBuilder_Marshal(t *testing.T) {
 		{
 			name:  "Pointer to interface struct containing interface{}(string) and any(int)",
 			value: &iFace,
-			want:  wantPtrValue(`iFaceStruct`, `iFaceStruct{iFace1:nil,iFace2:nil,}%s  var2 := any("Hello")%s  var3 := any(10)%s  var1.iFace1 = var2%s  var1.iFace2 = var3`, "\n", "\n", "\n", "\n"),
+			want:  wantPtrValue(`iFaceStruct`, `iFaceStruct{iFace1:"Hello",iFace2:10,}`),
 		},
 		{
 			name:  "nil",
@@ -132,12 +132,12 @@ func TestNodeBuilder_Marshal(t *testing.T) {
 		{
 			name:  "Slice of `any` containing 1,2,3",
 			value: []any{1, 2, 3},
-			want:  wantValue(`[]any`, `[]any{nil,nil,nil,}%s  var2 := any(1)%s  var3 := any(2)%s  var4 := any(3)%s  var1[0] = var2%s  var1[1] = var3%s  var1[2] = var4`, "\n", "\n", "\n", "\n", "\n", "\n"),
+			want:  wantValue(`[]any`, `[]any{1,2,3,}`),
 		},
 		{
 			name:  "Slice of any containing \"Hello\", \"GoodBy\"",
 			value: []any{"Hello", "Goodbye"},
-			want:  wantValue(`[]any`, `[]any{nil,nil,}%s  var2 := any("Hello")%s  var3 := any("Goodbye")%s  var1[0] = var2%s  var1[1] = var3`, "\n", "\n", "\n", "\n"),
+			want:  wantValue(`[]any`, `[]any{"Hello","Goodbye",}`),
 		},
 	}
 	for _, tt := range tests {
